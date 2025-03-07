51ab40cad484b4e3228515ef626718fc4b8620d7
Energy <179689881+locked-chest@users.noreply.github.com>
2025-03-07 06:20:03 +0000

    fix(engine): produce public.pem when rotating keys (#18)
    Co-authored-by: sentientflare <179689881+sentientflare@users.noreply.github.com>

--- webclient/rsa.ts
+++ webclient/rsa.ts
@@ -6,7 +6,9 @@ fs.writeFileSync('.env', '');
 
 const key = forge.pki.rsa.generateKeyPair(1024); // RS3/OSRS still uses 1024 today
 const privkey = forge.pki.privateKeyToPem(key.privateKey);
+const pubkey = forge.pki.publicKeyToPem(key.publicKey);
 fs.writeFileSync('private.pem', privkey);
+fs.writeFileSync('public.pem', pubkey);
 
 fs.appendFileSync('.env', 'LOGIN_RSAE=' + key.publicKey.e.toString(10) + '\n');
 fs.appendFileSync('.env', 'LOGIN_RSAN=' + key.publicKey.n.toString(10) + '\n');
